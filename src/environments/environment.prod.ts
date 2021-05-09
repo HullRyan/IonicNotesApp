507551eb978b77858242ff5c19107339027b0029
Ryan Hull <60150756+HullRyan@users.noreply.github.com>
2021-05-09 07:06:34 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyAu5BjUoO7HP4_EhE0jJ5wT2ow9d-3QKH4',
-    authDomain: 'ionic-notes-app.firebaseapp.com',
-    projectId: 'ionic-notes-app',
-    storageBucket: 'ionic-notes-app.appspot.com',
-    messagingSenderId: '178654441281',
-    appId: '1:178654441281:web:ac41ecda4ee08ef7347bc0',
-    measurementId: 'G-2JTHGVSX76',
+    //Insert your firebase api here
   },
 };
